7c50baf1c2680c731f201f357c5b2ab590e3c040
Othmane Abderrazik <othmane232004@gmail.com>
2025-02-23 23:10:00 +0000

    pages's config

--- src/main/java/ma/mycomp/demo/configurer/DemoWebMvcConfigurer.java
+++ src/main/java/ma/mycomp/demo/configurer/DemoWebMvcConfigurer.java
@@ -1,4 +1,21 @@
 package ma.mycomp.demo.configurer;
 
-public class DemoWebMvcConfigurer {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class DemoWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver pageableResolver = new PageableHandlerMethodArgumentResolver();
+        pageableResolver.setFallbackPageable(PageRequest.of(0, 3));
+        resolvers.add(pageableResolver);
+    }
 }

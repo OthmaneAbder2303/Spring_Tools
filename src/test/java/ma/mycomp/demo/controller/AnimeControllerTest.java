2f4f05c171b3bda3414d72257ec20d2c0db04f85
Othmane Abderrazik <othmane232004@gmail.com>
2025-03-06 11:29:48 +0000

    testing listAllAnimes

--- src/test/java/ma/mycomp/demo/controller/AnimeControllerTest.java
+++ src/test/java/ma/mycomp/demo/controller/AnimeControllerTest.java
@@ -3,14 +3,18 @@ package ma.mycomp.demo.controller;
 import ma.mycomp.demo.domain.Anime;
 import ma.mycomp.demo.service.AnimeService;
 import ma.mycomp.demo.util.AnimeCreator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -32,8 +36,14 @@ class AnimeControllerTest {
     }
 
     @Test
-    public void test() {
+    @DisplayName("getAnimes return a pageable list of animes when successful")
+    public void getAnimes_ReturnsListOfAnimesInsidePageOfObject_When_Successful() {
+        String expectedName = AnimeCreator.createValidAnime().getName();
+        Page<Anime> animePage = animeController.getAnimes(null).getBody();
 
+        Assertions.assertThat(animePage).isNotNull();
+        Assertions.assertThat(animePage.toList()).isNotEmpty();
+        Assertions.assertThat(expectedName).isEqualTo(animePage.toList().get(0).getName());
     }
 
 }

e4869823de3fe8344563fb234f131ee5851699d2
Othmane Abderrazik <othmane232004@gmail.com>
2025-03-06 16:07:53 +0000

    testing controller

--- src/test/java/ma/mycomp/demo/controller/AnimeControllerTest.java
+++ src/test/java/ma/mycomp/demo/controller/AnimeControllerTest.java
@@ -33,6 +33,16 @@ class AnimeControllerTest {
         PageImpl<Anime> animePage = new PageImpl<>(List.of(AnimeCreator.createValidAnime()));
         BDDMockito.when(animeServiceMocked.findAll(ArgumentMatchers.any()))
                 .thenReturn(animePage);
+
+        BDDMockito.when(animeServiceMocked.findById(ArgumentMatchers.anyInt()))
+                .thenReturn(AnimeCreator.createValidAnime());
+
+        BDDMockito.when(animeServiceMocked.findByName(ArgumentMatchers.any()))
+                .thenReturn(List.of(AnimeCreator.createValidAnime()));
+
+        BDDMockito.when(animeServiceMocked.save(AnimeCreator.createAnimeToBeSaved()))
+                .thenReturn(AnimeCreator.createValidAnime());
+
     }
 
     @Test
@@ -44,6 +54,38 @@ class AnimeControllerTest {
         Assertions.assertThat(animePage).isNotNull();
         Assertions.assertThat(animePage.toList()).isNotEmpty();
         Assertions.assertThat(expectedName).isEqualTo(animePage.toList().get(0).getName());
+    }
+
+    @Test
+    @DisplayName("getAnimeById return an anime when successful")
+    public void getAnimeById_ReturnsAnime_When_Successful() {
+        int expectedId = AnimeCreator.createValidAnime().getId();
+        Anime anime = animeController.getAnimeById(expectedId).getBody();
+
+        Assertions.assertThat(anime).isNotNull();
+        Assertions.assertThat(expectedId).isEqualTo(anime.getId());
+    }
+
+    @Test
+    @DisplayName("getAnimeByName return an anime when successful")
+    public void getAnimeByName_ReturnsAnime_When_Successful() {
+        String expectedName = AnimeCreator.createValidAnime().getName();
+        List<Anime> animes = animeController.getAnimeByName(expectedName).getBody();
+
+        Assertions.assertThat(animes).isNotNull();
+        Assertions.assertThat(animes).isNotEmpty();
+        Assertions.assertThat(expectedName).isEqualTo(animes.get(0).getName());
+    }
+
+    @Test
+    @DisplayName("createAnime return an anime when successful")
+    public void save_CreatesAnime_When_Successful() {
+        Anime anime = AnimeCreator.createAnimeToBeSaved();
+        Anime savedAnime = animeController.createAnime(anime).getBody();
+
+        Assertions.assertThat(savedAnime).isNotNull();
+        Assertions.assertThat(savedAnime.getName()).isNotEmpty();
+        Assertions.assertThat(savedAnime.getName()).isEqualTo(anime.getName());
     }
 
 }

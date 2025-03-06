927a0b9a3897eb9245c56622ae64122e151f6fd3
Othmane Abderrazik <othmane232004@gmail.com>
2025-03-06 01:26:59 +0000

    setting up mocked animeService

--- src/test/java/ma/mycomp/demo/controller/AnimeControllerTest.java
+++ src/test/java/ma/mycomp/demo/controller/AnimeControllerTest.java
@@ -2,13 +2,18 @@ package ma.mycomp.demo.controller;
 
 import ma.mycomp.demo.domain.Anime;
 import ma.mycomp.demo.service.AnimeService;
+import ma.mycomp.demo.util.AnimeCreator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,7 +26,9 @@ class AnimeControllerTest {
 
     @BeforeEach
     public void setUp() {
-       // new PageImpl<Anime>()
+        PageImpl<Anime> animePage = new PageImpl<>(List.of(AnimeCreator.createValidAnime()));
+        BDDMockito.when(animeServiceMocked.findAll(ArgumentMatchers.any()))
+                .thenReturn(animePage);
     }
 
     @Test
